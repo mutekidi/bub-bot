79a2fa2453628aa7ec44ada813bf3f7c0a8436a6
mutekidi <hannahbragelman@gmail.com>
2022-06-29 11:07:00 -0500

    Add project files.

---
+++ bub-bot/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace bub_bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var token = "";
+
+            using(StreamReader r = new StreamReader("appsettings.json"))
+            {
+                string json = r.ReadToEnd();
+                var data = JsonConvert.DeserializeObject<appsettings>(json);
+                token = data?.token ?? "";
+            }
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");         
+
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ bub-bot/appsettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bub_bot
+{
+    public class appsettings
+    {
+        public string token { get; set; }
+    }
+}
